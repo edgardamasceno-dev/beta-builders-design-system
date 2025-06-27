6fe6308daaf65f60bcb9f379dd79a3708b5928bc
edgardamasceno-dev <edgardamasceno-dev@users.noreply.github.com>
2025-06-27 15:19:37 +0000

    deploy: 942369c9167a15337cece653af8d8a577a89a8da

--- assets/react-18-Bjec_6kz.js
+++ assets/react-18-Bjec_6kz.js
@@ -1,4 +1,4 @@
-import{a as k1,r as Fc}from"./iframe-BzH6TvRE.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as k1,r as Fc}from"./iframe-D41I-0-3.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
